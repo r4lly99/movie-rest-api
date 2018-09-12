1e87fe6d4ab94ef8023aae5a813b3498af2f8fb6
mohd.rully.k <mohd.rully.k@gmail.com>
2018-09-12 09:45:53 +0700

    First Init

---
+++ app.go
@@ -0,0 +1,211 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	. "github.com/r4lly99/movie-rest-api/config"
+	. "github.com/r4lly99/movie-rest-api/dao"
+	. "github.com/r4lly99/movie-rest-api/models"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var config = Config{}
+var dao = MoviesDAO{}
+
+type Person struct {
+	Results []struct {
+		Gender string `json:"gender"`
+		Name   struct {
+			Title string `json:"title"`
+			First string `json:"first"`
+			Last  string `json:"last"`
+		} `json:"name"`
+		Location struct {
+			Street      string `json:"street"`
+			City        string `json:"city"`
+			State       string `json:"state"`
+			Postcode    int    `json:"postcode"`
+			Coordinates struct {
+				Latitude  string `json:"latitude"`
+				Longitude string `json:"longitude"`
+			} `json:"coordinates"`
+			Timezone struct {
+				Offset      string `json:"offset"`
+				Description string `json:"description"`
+			} `json:"timezone"`
+		} `json:"location"`
+		Email string `json:"email"`
+		Login struct {
+			UUID     string `json:"uuid"`
+			Username string `json:"username"`
+			Password string `json:"password"`
+			Salt     string `json:"salt"`
+			Md5      string `json:"md5"`
+			Sha1     string `json:"sha1"`
+			Sha256   string `json:"sha256"`
+		} `json:"login"`
+		Dob struct {
+			Date time.Time `json:"date"`
+			Age  int       `json:"age"`
+		} `json:"dob"`
+		Registered struct {
+			Date time.Time `json:"date"`
+			Age  int       `json:"age"`
+		} `json:"registered"`
+		Phone string `json:"phone"`
+		Cell  string `json:"cell"`
+		ID    struct {
+			Name  string `json:"name"`
+			Value string `json:"value"`
+		} `json:"id"`
+		Picture struct {
+			Large     string `json:"large"`
+			Medium    string `json:"medium"`
+			Thumbnail string `json:"thumbnail"`
+		} `json:"picture"`
+		Nat string `json:"nat"`
+	} `json:"results"`
+	Info struct {
+		Seed    string `json:"seed"`
+		Results int    `json:"results"`
+		Page    int    `json:"page"`
+		Version string `json:"version"`
+	} `json:"info"`
+}
+
+// GET list of movies
+func AllMoviesEndPoint(w http.ResponseWriter, r *http.Request) {
+	movies, err := dao.FindAll()
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusOK, movies)
+}
+
+// GET a movie by its ID
+func FindMovieEndpoint(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	movie, err := dao.FindById(params["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid Movie ID")
+		return
+	}
+	respondWithJson(w, http.StatusOK, movie)
+}
+
+// POST a new movie
+func CreateMovieEndPoint(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	var movie Movie
+	if err := json.NewDecoder(r.Body).Decode(&movie); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	movie.ID = bson.NewObjectId()
+	if err := dao.Insert(movie); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusCreated, movie)
+}
+
+// PUT update an existing movie
+func UpdateMovieEndPoint(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	var movie Movie
+	if err := json.NewDecoder(r.Body).Decode(&movie); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	if err := dao.Update(movie); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
+}
+
+// DELETE an existing movie
+func DeleteMovieEndPoint(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	var movie Movie
+	if err := json.NewDecoder(r.Body).Decode(&movie); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	if err := dao.Delete(movie); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
+}
+
+// Call another API https://randomuser.me/person
+func GetAnotherEndpoint(w http.ResponseWriter, r *http.Request) {
+	// url := fmt.Sprintf("http://apilayer.net/api/validate?access_key=YOUR_ACCESS_KEY&number=%s", safePhone)
+	url := "https://randomuser.me/api"
+
+	// Build the request
+	req, err := http.NewRequest("GET", url, nil)
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, err.Error())
+		return
+	}
+
+	defer resp.Body.Close()
+	// Fill the record with the data from the JSON
+	var record Person
+	// Use json.Decode for reading streams of JSON data
+
+	if err := json.NewDecoder(resp.Body).Decode(&record); err != nil {
+		log.Println(err)
+	}
+
+	respondWithJson(w, http.StatusOK, record.Results)
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondWithJson(w, code, map[string]string{"error": msg})
+}
+
+func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+// Parse the configuration file 'config.toml', and establish a connection to DB
+func init() {
+	config.Read()
+
+	dao.Server = config.Server
+	dao.Database = config.Database
+	dao.Connect()
+}
+
+// Define HTTP request routes
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/movies", AllMoviesEndPoint).Methods("GET")
+	r.HandleFunc("/movies", CreateMovieEndPoint).Methods("POST")
+	r.HandleFunc("/movies", UpdateMovieEndPoint).Methods("PUT")
+	r.HandleFunc("/movies", DeleteMovieEndPoint).Methods("DELETE")
+	r.HandleFunc("/movies/{id}", FindMovieEndpoint).Methods("GET")
+	r.HandleFunc("/call", GetAnotherEndpoint).Methods("GET")
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		log.Fatal(err)
+	}
+}
